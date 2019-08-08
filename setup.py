096a9c897d2a41d1f4c7b64598c97bdf3578c287
kkaleta <kamilkaleta77@gmail.com>
2019-08-08 10:22:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/kkaleta/OctoBNK"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-#plugin_requires = ["requests", "pushbullet.py"]
+plugin_requires = ["requests", "pushbullet.py"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
